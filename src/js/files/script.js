39be9e3ed4aff4d21634c4fdf9b1d8523e3bda4a
Taras Samoilov <tarsam74@gmail.com>
2023-08-17 16:01:45 +0300

    I am working on the header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,25 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	// let body=document.querySelector('body');
+	if (document.documentElement.classList.contains('touch')) {
+		let arrow=document.querySelectorAll('.arrow');
+		for (let i=0; i<arrow.length; i++){
+			let thisLink=arrow[i].previousElementSibling;
+			let subMenu=arrow[i].nextElementSibling;
+			let thisArrow=arrow[i];
+	
+			thisLink.classList.add('parent');
+			arrow[i].addEventListener('click', function(){
+				subMenu.classList.toggle('open');
+				thisArrow.classList.toggle('active');
+			});
+		}
+	}	
+});
+
+
+
