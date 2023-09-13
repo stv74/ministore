63dc5a69d7b89533163aec6c1aaf296b1965364f
Taras Samoilov <tarsam74@gmail.com>
2023-09-13 19:26:30 +0300

    Work on the section Prodacts

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -105,6 +105,82 @@ function initSliders() {
             on: {},
         });
     }
+    if (document.querySelector(".products__slider")) {
+        // Вказуємо склас потрібного слайдера
+        // Створюємо слайдер
+        new Swiper(".products__slider", {
+            // Вказуємо склас потрібного слайдера
+            // Підключаємо модулі слайдера
+            // для конкретного випадку
+            modules: [Pagination],
+            observer: true,
+            observeParents: true,
+            slidesPerView: 4,
+            spaceBetween: 20,
+            autoHeight: true,
+            speed: 800,
+
+            //touchRatio: 0,
+            //simulateTouch: false,
+            //loop: true,
+            //preloadImages: false,
+            //lazy: true,
+
+            /*
+			// Ефекти
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+            // Пагінація
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+
+            // Скроллбар
+            /*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+            // Кнопки "вліво/вправо"
+            // navigation: {
+            //     prevEl: ".swiper-button-prev",
+            //     nextEl: ".swiper-button-next",
+            // },
+            /*
+			// Брейкпоінти
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+            // Події
+            on: {},
+        });
+    }
 }
 // Скролл на базі слайдера (за класом swiper scroll для оболонки слайдера)
 function initSlidersScroll() {
